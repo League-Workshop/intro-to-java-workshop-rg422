45049ed81693af265085b66dda3f46e0e61405d4
AnkitBisain <league@WTS19.attlocal.net>
2017-08-22 14:54:51 -0700

    finished day 1 projects on day 2

--- src/day1/robot/FourSquare.java
+++ src/day1/robot/FourSquare.java
@@ -6,29 +6,31 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-	
+	Robot robot = new Robot ();
 
 	void go() {
+		robot.penDown();
 		// 4. Make the robot move as fast as possible
-
+		robot.setSpeed(10);
 		// 5. Set the pen width to 5
-
+		robot.setPenWidth(5);
 		// 6. Do steps #7 to #8 four times...
-
+		for (int i = 0; i < 4; i++) {
 			// 7. Set the pen color to random
-	
+			robot.setRandomPenColor();
 			// 1. Call the drawSquare() method
-	
+		drawSquare();
 			// 8. Turn the robot 90 degrees to the right
-
+		robot.turn(90);
+		}
+		robot.move(100);
 	}
-
 	/* 3. Fill in the code to draw a square inside the method below. */
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
-
-		
-		
+		for (int i = 0; i < 4; i++) {
+		robot.move(50);
+		robot.turn(90);
+		}
 	}
 
 	public static void main(String[] args) {
--- src/day1/robot/Spiral.java
+++ src/day1/robot/Spiral.java
@@ -12,16 +12,20 @@ public class Spiral {
 		// 3. Set the robot to go at max speed (10)
 		robot.setSpeed(10);
 		// 4. Do the following (steps 6-9) 75 times
+		for (int i = 0; i < 75; i++) {
+			
 		
 			// 7. Change the pen color to random
-	
+		robot.setRandomPenColor();
 			// 6. Move the robot 5 times the current line number you are drawing (5*i)
 		robot.move(5*i);
 			// 2. Turn the robot 1/3 of 360 degrees to the right
 		robot.turn(360/3);
 			// 8. Change the number of sides to 7 (don’t add a new line of code for this one!)
-	
+		
 			// 9. Set the pen width to i
-
-	}
-}
+		robot.setPenWidth(i);
+		
+		}//ending for loop
+	}//ending static void
+}//ending public class sprial
