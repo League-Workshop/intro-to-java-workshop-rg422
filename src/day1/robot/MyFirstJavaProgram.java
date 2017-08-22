52fb66c251d461a4ba39a3a03f829e811751e4f2
100297 <league@WTS7.attlocal.net>
2017-08-22 13:54:16 -0700

    Day 2

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -1,15 +1,41 @@
 package day1.robot;
 
 import java.awt.Color;
+import java.util.Iterator;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
-		
-		// START HERE
 
-	
+		// START HERE
+		//Create a new robot
+		Robot dummy = new Robot();
+		//Set Speed
+		dummy.setSpeed(10);
+		//Set Pen Color
+		dummy.setPenColor(Color.BLUE);
+		//Put Pen down
+		dummy.penDown();
+		//Set Robot starting point
+		dummy.setX(200);
+		//Begin Loop
+		//i < ? to and the 360 / ? replace ? to change the shape
+		for (int i = 0; i < 4; i++) {
+			dummy.move(50);
+			dummy.turn(360 / 4);
+
+		}
+		//Pen up
+		dummy.penUp();
+		//Move out of the way
+		dummy.move(100);
 		
-		
+		//Create a new robot
+		Robot smarty = new Robot();
+		smarty.moveTo(200, 100);
+		smarty.turn(180);
+		smarty.penUp();
+		smarty.moveTo(200, 75);
 	}
 }
